7df1acc936bed1e661178cf4dad1ec3832765550
AndreyKdo <andreypcd@gmail.com>
2026-07-29 16:43:03 -0600

    MOD scripts SQL init

---
+++ database/init/03-data.sql
@@ -0,0 +1,18 @@
+USE DockerWebinar;
+GO
+
+INSERT INTO Clientes
+VALUES
+
+('101010101',
+'Juan Pérez',
+250000),
+
+('202020202',
+'Ana López',
+135000),
+
+('303030303',
+'Carlos Mora',
+420000);
+GO
